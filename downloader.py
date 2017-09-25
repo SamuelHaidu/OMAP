f73e3686238ffe76b9ca0e5abdf180b1e95861a6
SamuelHaidu <31284013+SamuelHaidu@users.noreply.github.com>
2017-09-25 12:04:22 -0300

    Add m4a download
    "tp" in __init__ is type of download m4a or ogg.

--- downloader.py
+++ downloader.py
@@ -3,17 +3,24 @@ import pafy
 import os
 sdcard = '/mnt/sdcard/'
 class Downloader(Thread):
-    def __init__(self, url, callback=None):
+    def __init__(self, url, tp='ogg', callback=None):
         Thread.__init__(self)
         self.url = url
+        self.tp = tp
         self.callback = callback
     
     def run(self):
         self.video = pafy.new(self.url)
-        if self.callback:
-            self.video.getbestaudio().download(callback=self.callback,
+        if self.callback and self.tp == 'ogg':
+            self.video.getbestaudio(preftype='webm').download(callback=self.callback,
                                                filepath=sdcard+'OMAP/Download'+self.video.title+'.ogg', 
                                                quiet=True)
+            print 'Downloaded in ' +self.tp
+        elif self.callback and self.tp == 'm4a':
+            self.video.getbestaudio(preftype='m4a').download(callback=self.callback,
+                                                   filepath=sdcard+'OMAP/Download'+self.video.title+'.m4a', 
+                                                   quiet=True)
+            print 'Downloaded in ' +self.tp      
         else: 
-            self.video.getbestaudio().download(filepath=sdcard+'OMAP/Download'+self.video.title+'.ogg',
+            self.video.getbestaudio(preftype='webm').download(filepath=sdcard+'OMAP/Download'+self.video.title+'.ogg',
                                                quiet=False)
